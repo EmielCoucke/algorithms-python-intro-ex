14dcce9c11e4841f11ac0c6cd75316d4c7e41d26
Jarno <jarno.mechele@gmail.com>
2021-10-14 09:53:49 +0200

    changed Lucas => lucas

--- second.py
+++ second.py
@@ -1,9 +1,9 @@
-def Lucas(n):
+def lucas(n):
     if n==0:
         return 2
     if n==1:
         return 1
-    return Lucas(n-1) + Lucas(n-2)
+    return lucas(n-1) + lucas(n-2)
 
 for i in range(0,32):
-    print(Lucas(i))
+    print(lucas(i))
